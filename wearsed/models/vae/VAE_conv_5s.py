3033cef9a73d649282242eeb55928edfdc0a0017
Julian Bock von Wülfingen <julianhendrik.bvw@gmail.com>
2024-12-23 17:43:17 +0100

    Added a convolutional VAE with 5s winddow

---
+++ wearsed/models/vae/VAE_conv_5s.py
@@ -0,0 +1,60 @@
+'''
+Variational Autoencoder (VAE) for down sampling high frequency signals like `Pleth`
+'''
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class VAE(nn.Module):
+    def __init__(self, input_dim=5*256, latent_dim=8, output_dim=256):
+        super(VAE, self).__init__()
+        
+        # Encoder
+        self.enc_conv1 = nn.Conv1d(in_channels=1, out_channels=16, kernel_size=7, stride=5, padding=3)
+        self.enc_conv2 = nn.Conv1d(in_channels=16, out_channels=16, kernel_size=5, stride=2, padding=2)
+        self.enc_conv3 = nn.Conv1d(in_channels=16, out_channels=32, kernel_size=3, stride=2, padding=1)
+        self.enc_fc    = nn.Linear(32*64, 256)
+        self.mu_layer = nn.Linear(256, latent_dim)
+        self.log_var_layer = nn.Linear(256, latent_dim)
+        
+        # Decoder
+        self.dec_fc1 = nn.Linear(latent_dim, 256)
+        self.dec_fc2 = nn.Linear(256, 2048)
+        self.dec_conv1 = nn.ConvTranspose1d(in_channels=32, out_channels=16, kernel_size=3, stride=2, padding=1, output_padding=1)
+        self.dec_conv2 = nn.ConvTranspose1d(in_channels=16, out_channels=1, kernel_size=5, stride=2, padding=2, output_padding=1)
+        self.tanh = nn.Tanh()
+
+    def encode(self, x):                  # [bs,     1280]
+        x = x.unsqueeze(1)                # [bs,  1, 1280]
+        x = F.relu(self.enc_conv1(x))     # [bs, 16,  256]
+        x = F.relu(self.enc_conv2(x))     # [bs, 16,  128]
+        x = F.relu(self.enc_conv3(x))     # [bs, 32,   64]
+        x = x.view(x.shape[0], -1)        # [bs,     2048]
+        x = F.relu(self.enc_fc(x))        # [bs,      256]
+        mu, log_var = self.mu_layer(x), self.log_var_layer(x)
+        return mu, log_var
+
+    def reparameterize(self, mu, log_var):
+        std = torch.exp(0.5 * log_var)    # Standard deviation
+        eps = torch.randn_like(std)       # Random noise
+        return mu + eps * std             # Reparameterized sample
+    
+    def decode(self, z):
+        x = F.relu(self.dec_fc1(z))       # [bs,     256]
+        x = F.relu(self.dec_fc2(x))       # [bs,    2048]
+        x = x.view(x.shape[0], 32, 64)    # [bs, 32,  64]
+        x = F.relu(self.dec_conv1(x))     # [bs, 16, 128]
+        x = self.tanh(self.dec_conv2(x))  # [bs,  1, 256]
+        return x.squeeze(1)               # [bs,     256]
+
+    def forward(self, x):
+        mu, log_var = self.encode(x)
+        z = self.reparameterize(mu, log_var)
+        recon = self.decode(z) * 6  # Scaling the tanh (range [-1, 1]) to [-6, 6]
+        return recon, mu, log_var
+
+def vae_loss(recon, x, mu, log_var):
+    recon_loss = F.mse_loss(recon, x, reduction='sum')                   # Reconstruction Loss
+    kl_loss = -0.5 * torch.sum(1 + log_var - mu.pow(2) - log_var.exp())  # KL-Divergence Loss
+    return recon_loss + kl_loss
---
+++ wearsed/training/vae/train_vae_conv_5s.py
@@ -0,0 +1,97 @@
+'''
+Train code for training the down sampling VAE
+'''
+
+from wearsed.dataset.WearSEDDataset import WearSEDDataset
+from wearsed.models.vae.VAE_conv_5s import VAE, vae_loss
+
+from argparse import ArgumentParser
+from tqdm import tqdm
+import pandas as pd
+import os
+
+import torch.nn.functional as F
+import torch
+
+parser = ArgumentParser(description='Train code for training the down sampling VAE with a window of 5x')
+parser.add_argument('--epochs', help='number of epochs', default=50, type=int)
+parser.add_argument('--batch-size', help='batch size', default=256, type=int)
+args = parser.parse_args()
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+SIGNAL = 'Pleth'
+FREQ = 256  # If any other signal is chosen with a different frequency, the code has to be adapted elsewhere too
+
+def get_batch(recording, batch_size):
+    signal = recording.psg[SIGNAL]
+    max_time = len(signal) - 5*FREQ
+    batch = []
+    for timepoint in torch.randint(0, max_time, (batch_size,)):
+        batch.append(torch.Tensor(signal[timepoint.item():timepoint.item()+5*FREQ].values))
+    return torch.stack(batch)
+
+# Output folder
+OUTPUT_DIR = f'wearsed/training/vae/output_conv_5s/'
+os.makedirs(OUTPUT_DIR, exist_ok=True)
+
+# Dataset
+full_dataset = WearSEDDataset(signals_to_read=[SIGNAL], return_recording=True)
+train_dataset, test_dataset = torch.utils.data.random_split(full_dataset, [0.8, 0.2])
+
+# Model and Optimizer
+model = VAE().to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+
+# Training Loop
+train_losses = []
+test_losses = []
+
+for epoch in range(args.epochs):
+
+    # Taining
+    model.train()
+    train_loss = 0
+    for recording in tqdm(train_dataset):
+        optimizer.zero_grad()
+
+        batch = get_batch(recording, batch_size=args.batch_size).to(device)
+        
+        # Forward pass
+        recon, mu, log_var = model(batch)
+        
+        # Loss computation
+        loss = vae_loss(recon, batch[:, 2*FREQ:3*FREQ], mu, log_var)
+        loss.backward()
+        train_loss += loss.item()
+        optimizer.step()
+    
+    # Testing
+    model.eval()
+    test_loss = 0
+    with torch.no_grad():
+        for recording in tqdm(test_dataset):
+
+            batch = get_batch(recording, batch_size=args.batch_size).to(device)
+
+            # Forward pass
+            recon, mu, log_var = model(batch)
+            
+            # Loss computation
+            recon_loss = F.mse_loss(recon, batch[:, 2*FREQ:3*FREQ], reduction='sum')
+            test_loss += recon_loss.item()
+    
+    print(f'Epoch {epoch + 1}, Train Loss: {train_loss / len(train_dataset):.4f}, Test MSE Loss: {test_loss / (len(test_dataset) * FREQ):.4f}')
+    train_losses.append(train_loss / len(train_dataset))
+    test_losses.append(test_loss / (len(test_dataset) * FREQ))
+
+    if epoch % 10 == 0:
+        torch.save(model.state_dict(), OUTPUT_DIR + f'/model_epoch_{epoch}.pth')
+
+# Save model and losses
+torch.save(model.state_dict(), OUTPUT_DIR + '/model_final.pth')
+losses = pd.DataFrame({'train_losses': train_losses, 'test_losses': test_losses})
+losses.to_csv(OUTPUT_DIR + '/losses.csv', index=False)
+
+print(losses)
+

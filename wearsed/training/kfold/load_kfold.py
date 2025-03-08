a0cced6bb35702432e5a2935dd8516b8ffd0c968
Julian Bock von Wülfingen <julianhendrik.bvw@gmail.com>
2025-03-08 18:00:17 +0100

    Added shuffling to the train and test sets

--- wearsed/training/kfold/load_kfold.py
+++ wearsed/training/kfold/load_kfold.py
@@ -3,8 +3,9 @@ Load a generated k-Fold for the dataset using NSRR or SOMNOLYZER scorings
 '''
 
 import json
+import numpy as np
 
-def get_fold(fold_name, fold_nr, path='wearsed/training/kfold/'):
+def get_fold(fold_name, fold_nr, seed=42, path='wearsed/training/kfold/'):
     with open(f'{path}{fold_name}.txt', 'r') as f:
         json_dump = f.read()
     class_folds = json.loads(json_dump)
@@ -17,5 +18,9 @@ def get_fold(fold_name, fold_nr, path='wearsed/training/kfold/'):
                 test_set += class_folds[class_id][class_fold_nr]
             else:
                 train_set += class_folds[class_id][class_fold_nr]
+
+    np.random.seed(seed)
+    np.random.shuffle(train_set)
+    np.random.shuffle(test_set)
 
     return train_set, test_set
